c791bb95ad0889d591b3e49e102f0d75c78b9f40
Juha Lindstedt <juha@pakastin.fi>
2024-09-14 15:31:02 +0300

    Updated graphics

--- render-cards.js
+++ render-cards.js
@@ -6,46 +6,15 @@ const puppeteer = require('puppeteer');
   await page.goto(`file://${__dirname}/src/cards.html`);
 
   await page.setViewport({
-    width: 100,
-    height: 140,
-    deviceScaleFactor: 4
+    width: 1100,
+    height: 700,
+    deviceScaleFactor: 3
   });
 
-  const cardsCount = await page.evaluate(() => {
-    document.body.style.background = 'transparent';
+  const path = 'dist/standard-deck/sprite.jpg';
 
-    const cards = document.querySelectorAll('.card');
-
-    for (let i = 0; i < cards.length; i++) {
-      const card = cards[i];
-
-      card.style.opacity = 0;
-      card.style.margin = 0;
-      card.style.position = 'absolute';
-      card.style.left = 0;
-      card.style.top = 0;
-    }
-
-    return cards.length;
-  });
-
-  for (let i = 0; i < cardsCount; i++) {
-    await page.evaluate((i) => {
-      const cards = document.querySelectorAll('.card');
-      const card = cards[i];
-
-      card.style.opacity = '';
-    }, i);
-    const path = i > 0 ? `dist/standard-deck/front-${i - 1}.png` : 'dist/standard-deck/back.png';
-    await page.screenshot({ path, omitBackground: true });
-    console.log(`rendered ${path}`);
-    await page.evaluate((i) => {
-      const cards = document.querySelectorAll('.card');
-      const card = cards[i];
-
-      card.style.opacity = 0;
-    }, i);
-  }
+  await page.screenshot({ path });
+  console.log(`rendered ${path}`);
 
   await browser.close();
 })();
